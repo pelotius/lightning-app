2ec7ef7c6af8791a79efcc0dad77c96d24003a3d
Valentine Wallace <valentine.m.wallace@gmail.com>
2018-09-28 11:20:42 -0700

    Hook up inactive channels in channels view.

--- src/component/notification.js
+++ src/component/notification.js
@@ -67,7 +67,9 @@ export const alertColor = type => {
     ? color.greenSig
     : type === 'error'
       ? color.pinkSig
-      : color.orangeSig;
+      : type === 'inactive'
+        ? color.greySig
+        : color.orangeSig;
 };
 
 const alertStyles = StyleSheet.create({
